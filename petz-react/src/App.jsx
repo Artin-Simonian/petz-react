b2f7493db040828f71bceef69691141cc962d613
Artin Simonian <artinsimonian@Artins-MacBook-Air.local>
2023-11-27 18:35:59 -0800

    adding pages

--- petz-react/src/App.jsx
+++ petz-react/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-   <div>
-    <h1>Hello World!</h1>
-   </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
