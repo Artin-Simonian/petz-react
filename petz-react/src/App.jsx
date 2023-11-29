74cdfe3ea075b4d574e40ddfebe4e404948ca4c1
Artin Simonian <artinsimonian@Artins-MacBook-Air.local>
2023-11-28 17:00:35 -0800

    debugging

--- petz-react/src/App.jsx
+++ petz-react/src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
