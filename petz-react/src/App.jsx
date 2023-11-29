633d6b03d5aff87cffcf4f80f62832078ddd95ba
Artin Simonian <artinsimonian@Artins-MacBook-Air.local>
2023-11-28 17:40:14 -0800

    debugging part 2

--- petz-react/src/App.jsx
+++ petz-react/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-import "./App.css";
+import About from "./pages/About/About";
 import HomePage from "./pages/HomePage/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ petz-react/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div><h1>hi</h1></div>
+  );
+}
