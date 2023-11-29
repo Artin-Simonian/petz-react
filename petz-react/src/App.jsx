06012d4d12b46bc3ec464b5d9d9fdc4c323722cb
Artin Simonian <artinsimonian@Artins-MacBook-Air.local>
2023-11-28 17:45:52 -0800

    adding NavBar

--- petz-react/src/App.jsx
+++ petz-react/src/App.jsx
@@ -3,17 +3,19 @@ import About from "./pages/About/About";
 import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
